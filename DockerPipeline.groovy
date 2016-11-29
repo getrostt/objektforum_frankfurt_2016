c14a76ac4ae455a1f32a592546c2b5b4d58036dc
Tobias Getrost <tobias.getrost@1und1.de>
2016-11-29 10:05:13 +0100

    added properties to keep only 3 builds to docker pipeline

--- DockerPipeline.groovy
+++ DockerPipeline.groovy
@@ -2,6 +2,10 @@ def mvnImgDocker = 'maven:3.3-jdk-8-alpine'
 //def gitUrl = 'https://github.com/wildfly/quickstart.git'
 def gitUrl = '/git/wildfly-quickstarts/.git'
 
+properties([
+  buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '3')),
+])
+
 stage('Commit') {
     node {
         def img = docker.image(mvnImgDocker)
